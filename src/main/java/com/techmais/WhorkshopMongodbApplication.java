e2ccf2a02f12b8a3fd89f853461e61e5685eb53b
Daniel Martins <techmaisart@gmail.com>
2018-06-27 13:00:53 -0300

    Projeto Criado

---
+++ src/main/java/com/techmais/WhorkshopMongodbApplication.java
@@ -0,0 +1,12 @@
+package com.techmais;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WhorkshopMongodbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WhorkshopMongodbApplication.class, args);
+	}
+}
